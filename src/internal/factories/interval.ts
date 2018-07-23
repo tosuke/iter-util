d28b8cf04d96abde96bdb1971f9e31ac29e25ae4
Tosuke <tasukeprg@gmail.com>
2018-07-23 16:25:40 +0900

    :wrench: fix

--- src/internal/factories/interval.ts
+++ src/internal/factories/interval.ts
@@ -3,7 +3,7 @@ import { timer } from '../utils'
 export async function* interval(ms: number): AsyncIterable<number> {
   let counter = 0
   while (true) {
-    yield counter++
     await timer(ms)
+    yield counter++
   }
 }
