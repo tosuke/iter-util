a743484fa374fc9d16176180e0c2c3c88ec91115
Tosuke <tasukeprg@gmail.com>
2018-07-27 12:03:40 +0900

    :gear: implement "toMap" operator

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ export * from './internal/factories/recurrence'
 // operators
 export * from '@/internal/operators/toArray'
 export * from '@/internal/operators/toObject'
+export * from '@/internal/operators/toMap'
 export * from './internal/operators/filter'
 export * from './internal/operators/forEach'
 export * from './internal/operators/map'
---
+++ src/internal/operators/toMap.test.ts
@@ -0,0 +1,27 @@
+import { pipe, delay, toMap } from '@'
+
+test('toMap sync', () => {
+  const map = new Map<number, string>()
+  map.set(0, 'foo')
+  map.set(1, 'bar')
+
+  const res = map.entries()[pipe](toMap)
+  expect(res).toEqual(expect.any(Map))
+  expect(res.get(0)).toBe('foo')
+  expect(res.get(1)).toBe('bar')
+})
+
+test('toMap async', async () => {
+  const map = new Map<number, string>()
+  map.set(0, 'foo')
+  map.set(1, 'bar')
+
+  const res = map.entries()[pipe](
+    delay(100),
+    toMap,
+  )
+  expect(res).resolves.toEqual(expect.any(Map))
+  const map2 = await res
+  expect(map2.get(0)).toBe('foo')
+  expect(map2.get(1)).toBe('bar')
+})
