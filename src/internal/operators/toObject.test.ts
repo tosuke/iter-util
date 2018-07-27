703ca0a7825d88dfa75dee17e9209abdf4bc0ffc
Tosuke <tasukeprg@gmail.com>
2018-07-27 12:27:18 +0900

    :wrench: fix tests for node6

--- src/internal/operators/toObject.test.ts
+++ src/internal/operators/toObject.test.ts
@@ -1,14 +1,21 @@
 import { pipe, delay, toObject } from '@'
 
 test('toObject sync', () => {
-  const res = Object.entries({ a: 1, b: 2 })[pipe](toObject)
+  const res = objectEntries({ a: 1, b: 2 })[pipe](toObject)
   expect(res).toEqual({ a: 1, b: 2 })
 })
 
 test('toObject async', () => {
-  const res = Object.entries({ a: 1, b: 2 })[pipe](
+  const res = objectEntries({ a: 1, b: 2 })[pipe](
     delay(100),
     toObject,
   )
   expect(res).resolves.toEqual({ a: 1, b: 2 })
 })
+
+function* objectEntries<T>(obj: { [key: string]: T }): Iterable<[string, T]> {
+  const keys = Object.keys(obj)
+  for (const key of keys) {
+    yield [key, obj[key]]
+  }
+}
