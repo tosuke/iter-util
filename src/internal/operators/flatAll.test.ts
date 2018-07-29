42f488c3e0667e7d2d7f8e31af7df14ff5409296
Tosuke <tasukeprg@gmail.com>
2018-07-30 00:27:00 +0900

    :gear: implement "flatAll" operator

---
+++ src/internal/operators/flatAll.test.ts
@@ -0,0 +1,9 @@
+import { pipe, toArray, flatAll } from '@'
+
+test('flatAll', () => {
+  const res = [[0, 1], [2, 3]][pipe](
+    flatAll,
+    toArray,
+  )
+  expect(res).toEqual([0, 1, 2, 3])
+})
---
+++ src/internal/operators/flatAll.ts
@@ -0,0 +1,7 @@
+import { flatMap } from './flatMap'
+
+type IterableType<Iter extends Iterable<any>> = Iter extends Iterable<infer T> ? T : never
+
+export function flatAll<Iter extends Iterable<any>>(iter: Iterable<Iter>): Iterable<IterableType<Iter>> {
+  return flatMap<Iter, IterableType<Iter>>(id => id)(iter)
+}
