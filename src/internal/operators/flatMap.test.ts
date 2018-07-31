4085a43cf8450ba0c8438297969a3183c2bfbd62
Tosuke <tasukeprg@gmail.com>
2018-07-31 17:15:03 +0900

    :wrench: improve "flatMap" operator

--- src/internal/operators/flatMap.test.ts
+++ src/internal/operators/flatMap.test.ts
@@ -1,9 +1,17 @@
 import { pipe, range, toArray, flatMap } from '@'
 
-test('flatMap', () => {
+test('flatMap Iterable', () => {
   const res = [0, 1, 2, 3][pipe](
     flatMap(x => range(1, x)),
     toArray(),
   )
   expect(res).toEqual([1, 1, 2, 1, 2, 3])
+})
+
+test('flatMap Primitive', () => {
+  const res = [0, 1, 2, 3][pipe](
+    flatMap(x => x),
+    toArray(),
+  )
+  expect(res).toEqual([0, 1, 2, 3])
 })
--- src/internal/operators/flatMap.ts
+++ src/internal/operators/flatMap.ts
@@ -1,11 +1,18 @@
+import { isIterable } from '../utils'
+
 type FlatMapFunction<T, R> = (iter: Iterable<T>) => Iterable<R>
 
-export function flatMap<T, R>(f: (x: T) => Iterable<R>): FlatMapFunction<T, R> {
+export function flatMap<T, R>(f: (x: T) => Iterable<R> | R): FlatMapFunction<T, R> {
   return (iter: Iterable<T>) => flatMapImpl(iter, f)
 }
 
-function* flatMapImpl<T, R>(iter: Iterable<T>, f: (x: T) => Iterable<R>): Iterable<R> {
+function* flatMapImpl<T, R>(iter: Iterable<T>, f: (x: T) => Iterable<R> | R): Iterable<R> {
   for (const value of iter) {
-    yield* f(value)
+    const ret = f(value)
+    if (isIterable(ret)) {
+      yield* ret
+    } else {
+      yield ret
+    }
   }
 }
